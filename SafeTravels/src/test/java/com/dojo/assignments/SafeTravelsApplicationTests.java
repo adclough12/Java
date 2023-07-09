4a649d0de728a499ae1bf2c0c4f1e3d77237b988
Antonio Clough <129467848+adclough12@users.noreply.github.com>
2023-07-09 10:03:50 -0500

    Add files via upload

---
+++ SafeTravels/src/test/java/com/dojo/assignments/SafeTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.assignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTravelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
